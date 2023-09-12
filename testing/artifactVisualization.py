6c1f75bec78d183f42b9a3480d29dc32fcf7aa81
NB <nbryson2000@gmail.com>
2023-09-12 10:19:24 -0500

    EEG and State Visualization figures and data preprocessing

---
+++ testing/artifactVisualization.py
@@ -0,0 +1,11 @@
+from lib.processDat import processDat
+from pathlib import Path
+BCI2kPath = Path("C:\BCI2000.x64\data")
+dataPath = BCI2kPath / "NoahTesting000" /"NoahTestingS000R04.dat"
+t = processDat(dataPath)
+# t.plotStimOn()
+# t.visualizeRaw2Col()
+t.VisualizeRawOffset()
+t.psds()
+t.plotAllStates()
+t.show()
